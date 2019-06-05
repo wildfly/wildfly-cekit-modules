03c9f8e2ead5d119fb468058e08312fdd449a6ee
Yeray Borges <yborgess@redhat.com>
2019-06-05 09:20:09 +0100

    Added common scripts for S2I to enable Drivers

---
+++ jboss/container/wildfly/s2i/bash/configure.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Configure module
+set -e
+
+SCRIPT_DIR=$(dirname $0)
+ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
+
+chown -R jboss:root $SCRIPT_DIR
+chmod -R ug+rwX $SCRIPT_DIR
+chmod ug+x ${ARTIFACTS_DIR}/opt/jboss/container/wildfly/s2i/*
+
+pushd ${ARTIFACTS_DIR}
+cp -pr * /
+popd
+
+ln -s /opt/jboss/container/wildfly/s2i/install-common/install-common.sh /usr/local/s2i/install-common.sh
+chown -h jboss:root /usr/local/s2i/install-common.sh
