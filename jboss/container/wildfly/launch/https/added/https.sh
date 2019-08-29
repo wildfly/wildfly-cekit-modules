29042d072c88f7d484e1e1cade0173efc8a4e9e8
Kabir Khan <kkhan@redhat.com>
2019-08-29 17:54:49 +0100

    Fail fast if certain pre-conditions happen in the https script

--- jboss/container/wildfly/launch/https/added/https.sh
+++ jboss/container/wildfly/launch/https/added/https.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # only processes a single environment as the placeholder is not preserved
 
 source $JBOSS_HOME/bin/launch/logging.sh
@@ -72,7 +73,8 @@ function configureSslXml() {
 }
 
 function configureSslCli() {
-  local ssl_resource="/core-service=management/security-realm=ApplicationRealm/server-identity=ssl"
+  local app_realm_resource="/core-service=management/security-realm=ApplicationRealm"
+  local ssl_resource="${app_realm_resource}/server-identity=ssl"
   local ssl_add="$ssl_resource:add(keystore-path=\"${HTTPS_KEYSTORE_DIR}/${HTTPS_KEYSTORE}\", keystore-password=\"${HTTPS_PASSWORD}\""
   if [ -n "$HTTPS_KEYSTORE_TYPE" ]; then
     ssl_add="${ssl_add}, keystore-provider=\"${HTTPS_KEYSTORE_TYPE}\""
@@ -80,14 +82,15 @@ function configureSslCli() {
   ssl_add="${ssl_add})"
 
   cat << EOF >> ${CLI_SCRIPT_FILE}
-  if (outcome == success) of ${ssl_resource}:read-resource
-      batch
-        ${ssl_resource}:remove
-        ${ssl_add}
-      run-batch
-  else
-      ${ssl_add}
+  if (outcome != success) of ${app_realm_resource}:read-resource
+    echo You have set the HTTPS_PASSWORD, HTTPS_KEYSTORE_DIR and HTTPS_KEYSTORE to add the ssl server-identity. Fix your configuration to contain the ${app_realm_resource} resource for this to happen. >> \${error_file}
+    exit
   end-if
+  if (outcome == success) of ${ssl_resource}:read-resource
+    echo You have set the HTTPS_PASSWORD, HTTPS_KEYSTORE_DIR and HTTPS_KEYSTORE to add the ssl server-identity. But this already exists in the base configuration. Fix your configuration. >> \${error_file}
+    exit
+  end-if
+  ${ssl_add}
 EOF
 }
 
@@ -97,19 +100,38 @@ function configureHttpsXml() {
 }
 
 function configureHttpsCli() {
+    # No subsystem is an error
     local xpath="\"//*[local-name()='subsystem' and starts-with(namespace-uri(), 'urn:jboss:domain:undertow:')]\""
-    local ret
-    testXpathExpression "${xpath}" "ret"
+    local ssRet
+    testXpathExpression "${xpath}" "ssRet"
+    if [ "${ssRet}" -ne 0 ]; then
+      echo "You have set HTTPS_PASSWORD, HTTPS_KEYSTORE_DIR and HTTPS_KEYSTORE to add an undertow https-listener. Fix your configuration to contain the undertow subsystem for this to happen." >> "${CLI_SCRIPT_ERROR_FILE}"
+      return
+    fi
 
-    if [ "${ret}" -eq 0 ]; then
+    # Not having any servers is an error
+    local serverNamesRet
+    # We grab the <server name="..."> attributes, and will use them later
+    local xpath="\"//*[local-name()='subsystem' and starts-with(namespace-uri(), 'urn:jboss:domain:undertow:')]/*[local-name()='server']/@name\""
+    testXpathExpression "${xpath}" "serverNamesRet"
+    if [ "${serverNamesRet}" -ne 0 ]; then
+      echo "You have set HTTPS_PASSWORD, HTTPS_KEYSTORE_DIR and HTTPS_KEYSTORE to add an undertow https-listener. Fix your configuration to contain at least one server in the undertow subsystem for this to happen." >> ${CLI_SCRIPT_ERROR_FILE}
+      return
+    fi
+
+    # Existing https-listener(s) is an error
+    local httpsListenersRet
+    local xpath="\"//*[local-name()='subsystem' and starts-with(namespace-uri(), 'urn:jboss:domain:undertow:')]/*[local-name()='server']/*[local-name()='https-listener']/@name\""
+    testXpathExpression "${xpath}" "httpsListenersRet"
+    if [ "${httpsListenersRet}" -eq 0 ]; then
+      echo "You have set HTTPS_PASSWORD, HTTPS_KEYSTORE_DIR and HTTPS_KEYSTORE to add https-listeners to your undertow servers, however at least one of these already contains an https-listener. Fix your configuration." >> "${CLI_SCRIPT_ERROR_FILE}"
+      return
+    fi
+
     cat << EOF >> ${CLI_SCRIPT_FILE}
     for serverName in /subsystem=undertow:read-children-names(child-type=server)
-        if (result == []) of /subsystem=undertow/server=\$serverName:read-children-names(child-type=https-listener)
-          /subsystem=undertow/server=\$serverName/https-listener=https:add(security-realm=ApplicationRealm, socket-binding=https, proxy-address-forwarding=true)
-        else
-          echo There is already an undertow https-listener for the '\$serverName' server so we are not adding it >> \${warning_file}
-        end-if
+      /subsystem=undertow/server=\$serverName/https-listener=https:add(security-realm=ApplicationRealm, socket-binding=https, proxy-address-forwarding=true)
     done
 EOF
-    fi
+
 }
