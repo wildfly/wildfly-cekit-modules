3d357eccd9a994be336cfaf67da60e18051bf85c
JF Denise <jdenise@redhat.com>
2019-09-16 11:35:01 +0200

    CLI support for login-modules-common.sh

--- jboss/container/wildfly/launch/security-domains/configure.sh
+++ jboss/container/wildfly/launch/security-domains/configure.sh
@@ -6,5 +6,7 @@ ADDED_DIR=${SCRIPT_DIR}/added
 
 cp -p ${ADDED_DIR}/launch/security-domains.sh ${JBOSS_HOME}/bin/launch/
 chmod ug+x ${JBOSS_HOME}/bin/launch/security-domains.sh
+cp -p ${ADDED_DIR}/launch/login-modules-common.sh ${JBOSS_HOME}/bin/launch/
+chmod ug+x ${JBOSS_HOME}/bin/launch/login-modules-common.sh
 
 
