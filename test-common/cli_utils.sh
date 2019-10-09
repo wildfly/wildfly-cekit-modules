5e3e9ab6137127c2ae47b5417c8f36b93cc03861
Yeray Borges <yborgess@redhat.com>
2019-10-09 17:39:04 +0100

    Test if  exist and if it is writeable

--- test-common/cli_utils.sh
+++ test-common/cli_utils.sh
@@ -4,6 +4,8 @@ normalize_spaces_new_lines() {
   output=$(printf '%s\n' "$output" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e '/^$/d')
   expected=$(printf '%s\n' "$expected" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e '/^$/d')
 
-  echo "${output}" > "${TMPDIR}"/output
-  echo "${expected}" > "${TMPDIR}"/expected
+  if [ -w "${TMPDIR:-/tmp}" ]; then
+    echo "${output}" > "${TMPDIR:-/tmp}"/output
+    echo "${expected}" > "${TMPDIR:-/tmp}"/expected
+  fi
 }
