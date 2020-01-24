da903c48746ab046f2f4009af9e97a0e89575e76
Yeray Borges <yborgess@redhat.com>
2020-01-24 15:23:57 +0000

    [issue-147] Encapsulate the steps to run a CLI operations into a simple function that can be invoked sourcing install-common.sh Fixes #147

--- jboss/container/wildfly/s2i/bash/artifacts/opt/jboss/container/wildfly/s2i/install-common/install-common.sh
+++ jboss/container/wildfly/s2i/bash/artifacts/opt/jboss/container/wildfly/s2i/install-common/install-common.sh
@@ -120,3 +120,19 @@ function configure_drivers(){
   )
 }
 
+function run_cli_script(){
+  if [ $# != 1 ]; then
+    echo "Usage: Script file parameter required."
+    return
+  fi
+
+  local cli_script=$1
+
+  if [ ! -f "${cli_script}" ]; then
+    echo "run_cli_script: Script file parameter "${cli_script}" does not exit."
+    return
+  fi
+
+  createConfigExecutionContext
+  exec_cli_scripts "${cli_script}"
+}
