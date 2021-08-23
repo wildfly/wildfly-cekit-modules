2e1b209ac714f037b240342534e5c61214ea48e9
JF Denise <jdenise@redhat.com>
2021-08-23 10:26:21 +0200

    Remove useless chown in deployment-scanner installation script

--- jboss/container/wildfly/launch/deployment-scanner/configure.sh
+++ jboss/container/wildfly/launch/deployment-scanner/configure.sh
@@ -7,5 +7,4 @@ ADDED_DIR=${SCRIPT_DIR}/added
 mkdir -p ${JBOSS_HOME}/bin/launch
 cp -r ${ADDED_DIR}/launch/deploymentScanner.sh ${JBOSS_HOME}/bin/launch
 
-chown jboss:root $JBOSS_HOME/bin/launch/deploymentScanner.sh
 chmod g+rwX $JBOSS_HOME/bin/launch/deploymentScanner.sh
