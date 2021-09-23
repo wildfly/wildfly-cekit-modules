2cb0081ec62ce9e4c244d46f9f4d9e6eaa479f5b
JF Denise <jdenise@redhat.com>
2021-09-23 20:15:35 +0200

    Introduce legacy s2i module to enable current s2i workflow

--- jboss/container/wildfly/s2i/bash/artifacts/opt/jboss/container/wildfly/s2i/assemble.sh
+++ jboss/container/wildfly/s2i/bash/artifacts/opt/jboss/container/wildfly/s2i/assemble.sh
@@ -8,6 +8,5 @@ source "${JBOSS_CONTAINER_WILDFLY_S2I_MODULE}/s2i-core-hooks"
 # inject our overridden maven_s2i_*() functions
 source "${JBOSS_CONTAINER_WILDFLY_S2I_MODULE}/maven-s2i-overrides"
 
-export MAVEN_ARGS_APPEND="${MAVEN_ARGS_APPEND} -Dcom.redhat.xpaas.repo.jbossorg"
 # invoke the build
 maven_s2i_build
---
+++ jboss/container/wildfly/s2i/legacy/artifacts/opt/jboss/container/wildfly/s2i/assemble.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+source "${JBOSS_CONTAINER_UTIL_LOGGING_MODULE}/logging.sh"
+source "${JBOSS_CONTAINER_MAVEN_S2I_MODULE}/maven-s2i"
+
+if [ -n "${GALLEON_PROVISION_LAYERS}" ]; then
+   if [ -z "${GALLEON_PROVISION_FEATURE_PACKS}" ]; then
+     log_error "GALLEON_PROVISION_FEATURE_PACKS env variable must be set when GALLEON_PROVISION_LAYERS is set"
+     exit 1
+   fi
+fi
+if [ -n "${GALLEON_PROVISION_FEATURE_PACKS}" ]; then
+   if [ -z "${GALLEON_PROVISION_LAYERS}" ]; then
+     log_error "GALLEON_PROVISION_LAYERS env variable must be set when GALLEON_PROVISION_FEATURE_PACKS is set"
+     exit 1
+   fi
+fi
+if [ -n "${GALLEON_PROVISION_FEATURE_PACKS}" ]; then
+  # Legacy s2i workflow integration
+  #For backward compatibility
+  export CONFIG_ADJUSTMENT_MODE=cli
+  source "${JBOSS_CONTAINER_WILDFLY_S2I_LEGACY_GALLEON_MODULE}/s2i-core-hooks"
+  source "${JBOSS_CONTAINER_WILDFLY_S2I_LEGACY_GALLEON_MODULE}/s2i_galleon"
+  
+  galleon_provision_server
+else
+  # include our overrides/extensions
+  source "${JBOSS_CONTAINER_WILDFLY_S2I_MODULE}/s2i-core-hooks"
+
+  # inject our overridden maven_s2i_*() functions
+  source "${JBOSS_CONTAINER_WILDFLY_S2I_MODULE}/maven-s2i-overrides"
+fi
+# invoke the build
+maven_s2i_build
