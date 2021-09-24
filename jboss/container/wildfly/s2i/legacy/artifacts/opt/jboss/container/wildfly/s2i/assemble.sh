5e799d733c47d5e4af9aacb71becfdcbd89f4b55
JF Denise <jdenise@redhat.com>
2021-09-24 18:48:03 +0200

    Allow to provision the default standalone config

--- jboss/container/wildfly/s2i/legacy/artifacts/opt/jboss/container/wildfly/s2i/assemble.sh
+++ jboss/container/wildfly/s2i/legacy/artifacts/opt/jboss/container/wildfly/s2i/assemble.sh
@@ -9,12 +9,7 @@ if [ -n "${GALLEON_PROVISION_LAYERS}" ]; then
      exit 1
    fi
 fi
-if [ -n "${GALLEON_PROVISION_FEATURE_PACKS}" ]; then
-   if [ -z "${GALLEON_PROVISION_LAYERS}" ]; then
-     log_error "GALLEON_PROVISION_LAYERS env variable must be set when GALLEON_PROVISION_FEATURE_PACKS is set"
-     exit 1
-   fi
-fi
+
 if [ -n "${GALLEON_PROVISION_FEATURE_PACKS}" ]; then
   log_warning "You have activated legacy s2i workflow by setting GALLEON_PROVISION_FEATURE_PACKS env variable."
   log_warning "This support is deprecated and will be removed in a future release. Provision and configure your server during s2i from your pom.xml file by using the dedicated Maven plugin."
