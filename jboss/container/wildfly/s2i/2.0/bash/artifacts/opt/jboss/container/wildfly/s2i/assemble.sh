14c1f28ab17c33f75628ac7082fe8dc836e7880a
Kabir Khan <kkhan@redhat.com>
2022-09-13 17:34:39 +0100

    Make galleon provisioning consumable without legacy s2i

--- jboss/container/wildfly/s2i/2.0/bash/artifacts/opt/jboss/container/wildfly/s2i/assemble.sh
+++ jboss/container/wildfly/s2i/2.0/bash/artifacts/opt/jboss/container/wildfly/s2i/assemble.sh
@@ -5,6 +5,10 @@ set -e
 source "${JBOSS_CONTAINER_UTIL_LOGGING_MODULE}/logging.sh"
 source "${JBOSS_CONTAINER_MAVEN_S2I_MODULE}/maven-s2i"
 
+if [ -f  "${JBOSS_CONTAINER_WILDFLY_S2I_GALLEON_MODULE}/s2i_galleon" ]; then
+  source "${JBOSS_CONTAINER_WILDFLY_S2I_GALLEON_MODULE}/s2i_galleon"
+fi
+
 # include our overrides/extensions
 source "${JBOSS_CONTAINER_WILDFLY_S2I_MODULE}/s2i-core-hooks"
 source "${JBOSS_CONTAINER_WILDFLY_S2I_MODULE}/maven-s2i-overrides"
