9b5e0466f65fa69c2ab8ff33df52f8e55d782a58
Jean-Francois Denise <jdenise@redhat.com>
2023-08-23 15:20:35 +0200

    Fix for CLOUD-4173 continuing

--- jboss/container/wildfly/launch/os/node-name/added/launch/openshift-node-name.sh
+++ jboss/container/wildfly/launch/os/node-name/added/launch/openshift-node-name.sh
@@ -7,8 +7,11 @@ function init_node_name() {
     else
       JBOSS_NODE_NAME="${HOSTNAME}"
     fi
-
-    # CLOUD-427: truncate transaction node-id JBOSS_TX_NODE_ID to the last 23 characters of the JBOSS_NODE_NAME
+  fi
+  # CLOUD-427: truncate transaction node-id JBOSS_TX_NODE_ID to the last 23 characters of the JBOSS_NODE_NAME
+  if [ ${#JBOSS_NODE_NAME} -gt 23 ]; then
     JBOSS_TX_NODE_ID=${JBOSS_NODE_NAME: -23}
+  else
+    JBOSS_TX_NODE_ID=${JBOSS_NODE_NAME}
   fi
 }
