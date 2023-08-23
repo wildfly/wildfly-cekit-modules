2bf59341cc405363eeba6c0dfb7b69879746a257
Jean-Francois Denise <jdenise@redhat.com>
2023-08-23 15:20:35 +0200

    Fix for CLOUD-4173 continuing

--- jboss/container/wildfly/run/bash/artifacts/opt/jboss/container/wildfly/run/run-utils.sh
+++ jboss/container/wildfly/run/bash/artifacts/opt/jboss/container/wildfly/run/run-utils.sh
@@ -68,8 +68,11 @@ function run_init_node_name() {
     else
       JBOSS_NODE_NAME="$(hostname)"
     fi
-
-    # CLOUD-427: truncate transaction node-id JBOSS_TX_NODE_ID to the last 23 characters of the JBOSS_NODE_NAME
+  fi
+  # CLOUD-427: truncate transaction node-id JBOSS_TX_NODE_ID to the last 23 characters of the JBOSS_NODE_NAME
+  if [ ${#JBOSS_NODE_NAME} -gt 23 ]; then
     JBOSS_TX_NODE_ID=${JBOSS_NODE_NAME: -23}
+  else
+    JBOSS_TX_NODE_ID=${JBOSS_NODE_NAME}
   fi
 }
